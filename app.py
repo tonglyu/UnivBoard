4548576550d48d6f6f2959a6a821e8e7fde28dfd
ZiweiYuan <ziweiyua@usc.edu>
2019-02-09 22:22:41 -0800

    framework

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import config
 
+app = Flask(__name__)
+app.config.from_object(config)
+Bootstrap(app)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def search():
+    return render_template('search.html')
+
+@app.route('/feature')
+def feature():
+    return render_template('subsection.html')
 
 if __name__ == '__main__':
     app.run()
