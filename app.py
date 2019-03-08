f9d004225118b7a8596d9a8c86e92a3192f57e76
ZiweiYuan <ziweiyua@usc.edu>
2019-03-08 12:16:34 -0800

    framework

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
-Bootstrap(app)
 
 @app.route('/')
 def search():
