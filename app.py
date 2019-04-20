483d91c0433fb1484d4ed2f63036b939bce249b9
ZiweiYuan <ziweiyua@usc.edu>
2019-04-19 18:41:44 -0700

    add pagination

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import config
 from elasticsearch import Elasticsearch
+from flask_paginate import Pagination, get_page_parameter
 
 app = Flask(__name__)
 app.config.from_object(config)
+results = {}
 
 #connect to our cluster
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
@@ -12,13 +14,26 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['Get', 'POST'])
 def search_programs():
-    keywords = request.form['keywords']
-    universities = request.values.getlist('univ')
-    departments = request.values.getlist('depart')
-    results = es.search(index="scu-program", body={"query": {"match": {'title':keywords}}})
-    return render_template('search.html', results=results["hits"]["hits"])
+    global results
+    if request.method == "POST":
+        keywords = request.form['keywords']
+        universities = request.values.getlist('univ')
+        departments = request.values.getlist('depart')
+        results = es.search(index="scu-program", body={"from" : 0, "size" : 50, "query": {"match": {'title':keywords}}})
+
+    # Pagination
+    page = int(request.args.get('page', 1))
+    per_page = 5
+    offset = (page - 1) * per_page
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+    results_for_render = results["hits"]["hits"][offset: offset + per_page]
+    pagination = Pagination(page=page, per_page=per_page, offset=offset, total=len(results["hits"]["hits"]), search=search, css_framework='bootstrap4')
+    return render_template('search.html', results=results_for_render, pagination=pagination)
 
 @app.route('/details')
 def show_details():
